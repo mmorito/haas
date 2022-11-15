4735fb2152192038a412a4e3d1176a447298542f
mmorito <morito@mnes.org>
2022-11-16 00:11:27 +0900

    remove: unnecessary files

--- shell/protoc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-protoc -I ./organization/proto \
-    --openapiv2_out=logtostderr=true:./organization/proto/haas/organization/ \
-    ./organization/proto/haas/organization/organization.proto
